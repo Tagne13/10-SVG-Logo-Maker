8b5aac6a1b89489fc071c23ce762946476c118e9
El Jefe <Tagne13@gmail.com>
2023-04-17 21:57:05 -0400

    made several fixes to code across all files with assist of tutor

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateLogo = require('./util/generateLogo.js');
+// const generateLogo = require('./util/generateLogo.js');
 const { wrap } = require('module');
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
 
 const questions = [
     {
@@ -27,12 +28,28 @@ const questions = [
     {
         type: 'input',
         message: 'What color would you like the SVG shape to be?',
-        name: 'shapeColor'
+        name: 'color'
     },
 ];
+var generateLogo = ("");
 
-function writeToFile(fileName, data) {
-    let content = generateLogo(data);
+function generateLogo(response) {
+    if (response.shape === 'Circle') {
+        let userLogo = new Circle (response.text, response.textColor, response.color)
+        return userLogo.render()
+    } 
+    if (response.shape === 'Square') {
+        let userLogo = new Square (response.text, response.textColor, response.color)
+        return userLogo.render()
+    }
+    if (response.shape === 'Triangle') {
+        let userLogo = new Triangle (response.text, response.textColor, response.color)
+        return userLogo.render()
+    }
+};
+
+function writeToFile(fileName, response) {
+    let content = generateLogo(response);
     fs.writeFile(fileName, content, function (error) {
         if (error) {
             return console.log(error);
@@ -43,11 +60,12 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-    inquirer.prompt(questions).then(function (data) {
+    inquirer.prompt(questions).then(function (response) {
         let fileName = "./samples/logo.svg";
-        writeToFile(fileName, data);
+        writeToFile(fileName, response);
     });
 }
 
 init();
 
+// module.exports = generateLogo;
